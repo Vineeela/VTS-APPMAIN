ec4dbd0c27cc2dc801d622be1a44df67bad76ee8
Ak981208 <104905216+Ak981208@users.noreply.github.com>
2022-05-09 10:27:22 +0530

    Revert "Added card and tab component"

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import Cardss from '../components/Cards/Card';
 import NavbarVts from '../components/navbar/Toolbar';
-import Tabbs from '../components/Tabs/Tabs';
 
  const Overview = () => {
   return (
     <div>
-
-        <div><NavbarVts/></div>
-        <div><Tabbs/></div>
-        <div><Cardss/></div>
+        <NavbarVts/>
+        
     </div>
   )
 };
